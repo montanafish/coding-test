8b91966c66df7d2a5fed7e3fb0daf65ddb95a36f
montanafish <mitch@officesato.com>
2018-09-12 13:50:01 +1000

    added index page to test styled-components

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class IndexPage extends React.Component {
+  render() {
+    return (
+      <div>Index</div>
+    );
+  }
+}
+
+export default IndexPage;
